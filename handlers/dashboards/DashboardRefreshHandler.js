f40c9a26b46f590fb258dff5af4c54e456e94ebc
archonxgames <archonxgames@gmail.com>
2024-01-16 01:28:06 +0800

    Added a catch block for handling errors when refreshing dashboards

--- handlers/dashboards/DashboardRefreshHandler.js
+++ handlers/dashboards/DashboardRefreshHandler.js
@@ -109,28 +109,25 @@ module.exports = {
 
 			dashboardSettings.forEach(async (dashboardSetting) => {
 				let guildId = dashboardSetting.guildId
-				let channel = await client.guilds.cache.get(guildId).channels.cache.get(dashboardSetting.dashboardChannelId)
-				
-				//delete all dashboards belonging to a channel if the channel no longer exists
-				if(channel === undefined) {
+				let channel = await client.guilds.cache.get(guildId).channels.cache.get(dashboardSetting.dashboardChannelId).catch(() => {
+					//delete all dashboards belonging to a channel if the channel no longer exists
 					Logger.info(`DashboardRefreshHandler.js`, 'refreshAll', 'The channel containing the dashboard no longer exists. Deleting all dashboards associated with the channel.')
-					await DashboardSetting.destroy({
+					DashboardSetting.destroy({
 						where: {
 							dashboardChannelId: dashboardSetting.dashboardChannelId
 						}
 					})
-				}
-				let message = await channel.messages.fetch(dashboardSetting.dashboardMessageId)
-
-				//delete dashboard if its bound message no longer exists
-				if(message === undefined) {
+				})
+				
+				let message = await channel.messages.fetch(dashboardSetting.dashboardMessageId).catch(() => {
+					//delete dashboard if its bound message no longer exists
 					Logger.info(`DashboardRefreshHandler.js`, 'refreshAll', 'The message containing the dashboard no longer exists. Deleting dashboard entry.')
-					await DashboardSetting.destroy({
+					DashboardSetting.destroy({
 						where: {
 							dashboardMessageId: dashboardSetting.dashboardMessageId
 						}
 					})
-				}
+				})
 
 				let result = await handleRefresh(dashboardSetting)
 				return await message.edit(result.message)
